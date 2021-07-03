e76a96fd2c03b8ec1966751b7bdf33e46ae5b788
liweisong-java <liweisong0628@gmail.com>
2021-07-03 23:41:46 +0800

    第12次提交

--- day06/src/collection/CollectionTest1.java
+++ day06/src/collection/CollectionTest1.java
@@ -1,5 +1,6 @@
 package collection;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -18,16 +19,16 @@ public class CollectionTest1 {
        Collection coll = new ArrayList();
        coll.add(123);
        coll.add(465);
-
        coll.add(new String("Tom"));
        coll.add(false);
+
        coll.add(456);
        coll.add(new Person("Jerry",20));
 
-       //contains(Object obj)
+       //contains(Object obj):判断当前集合中是否包含obj
        boolean contains = coll.contains(123);
        System.out.println(contains);
-
-       //
+       System.out.println(coll.contains(new String("Tom")));
+       System.out.println(coll.contains(new Person("Jerry",20)));//???  new的地方是堆  跟add出来的指向的地址值不同
    }
 }
